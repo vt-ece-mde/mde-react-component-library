2e97e93ac80c1dab30b4b9d216f4c67cd20d9a99
Alexander DeRieux <alexander.derieux@gmail.com>
2023-03-24 17:51:00 -0400

    Initial setup
    - Installs `typescript`
    - Installs `tailwind` for styling
    - Installs `rollup` for building and config
    - Adds `playground` development environment with NextJS
    - Adds `HelloWorld` component for initial testing

---
+++ playground/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { HelloWorld } from 'mde-react-component-library';
+
+export default function Page() {
+  return (
+    <div className="text-3xl">
+      <HelloWorld />
+    </div>
+  )
+}
---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function HelloWorld() {
+    return (<><div className="text-purple-500">Hello World!</div></>)
+}
